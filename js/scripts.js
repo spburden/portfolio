4d8d3b9ce3eb1448720aa3a68a041a193b9464e0
Stephen Burden <spburden@hotmail.com>
2017-03-13 20:09:05 -0700

    add language filter to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  
+
   $("#home").click(function() {
     $('html, body').animate({
       scrollTop: $(".pageOne").offset().top
@@ -24,4 +24,27 @@ $(document).ready(function() {
     }, 1500);
   });
 
+  $('select').on('change', function() {
+    // var language_array = array()
+    var language = this.value;
+    // alert(language);
+    if (language === "all") {
+      $('.col-sm-4').show();
+    } else if (language === "php") {
+      $('.col-sm-4').hide();
+      $('.php').show();
+    } else if (language === "js") {
+      $('.col-sm-4').hide();
+      $('.js').show();
+    } else if (language === "drupal") {
+      $('.col-sm-4').hide();
+      $('.drupal').show();
+    }
+    $('html, body').animate({
+      scrollTop: $(".pageThree").offset().top
+    }, 500);
+  });
+
 });
+
+// BACKEND //
