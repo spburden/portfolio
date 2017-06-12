fe1fd028355831f42dc19a699202c50dcb1b082a
Stephen Burden <spburden@hotmail.com>
2017-06-12 16:02:11 -0700

    dry up functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,20 @@
 $(document).ready(function() {
-
-  $("#home").click(function() {
+  $("nav a").click(function() {
+    var part = this.attributes['data-part'].value;
     $('html, body').animate({
-      scrollTop: $(".pageOne").offset().top
+      scrollTop: $("." + part).offset().top
     }, 1500);
   });
-
-  $("#about").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".pageTwo").offset().top
-    }, 1500);
-  });
-
-  $("#portfolio").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".pageThree").offset().top
-    }, 1500);
-  });
-
-  $("#contact").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".pageFour").offset().top
-    }, 1500);
-  });
-
   $('select').on('change', function() {
-    // var language_array = array()
     var language = this.value;
-    // alert(language);
-    // setTimeout(function(){
-      if (language === "all") {
-        $('.porto').fadeIn("slow");
-      } else if (language === "php") {
-        $('.porto').fadeOut("slow");
-        $('.php').fadeIn("slow");
-      } else if (language === "js") {
-        $('.porto').fadeOut("slow");
-        $('.js').fadeIn("slow");
-      } else if (language === "drupal") {
-        $('.porto').fadeOut("slow");
-        $('.drupal').fadeIn("slow");
-      }
-    // }, 1000);
+    if (language === "all") {
+      $('.porto').fadeIn("slow");
+    } else {
+      $('.porto').fadeOut("slow");
+      $('.' + language).fadeIn("slow");
+    }
     $('html, body').animate({
       scrollTop: $(".pageThree").offset().top
     }, 1000);
   });
-
 });
-
-// BACKEND //
